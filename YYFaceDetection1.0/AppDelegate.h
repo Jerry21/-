094614093b5697adcb0d2a8fbd23ca5b16b2cebc
yejunyou <ye_junyou@163.com>
2017-05-30 08:38:34 +0800

    Initial Commit

---
+++ YYFaceDetection1.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYFaceDetection1.0
+//
+//  Created by 叶俊有 on 2017/5/30.
+//  Copyright © 2017年 yejunyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYFaceDetection1.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYFaceDetection1.0
+//
+//  Created by 叶俊有 on 2017/5/30.
+//  Copyright © 2017年 yejunyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
